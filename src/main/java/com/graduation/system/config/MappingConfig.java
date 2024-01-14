b2b1f54660360ea0f267be5997dc5339bba2ad63
aleksanderstoyanov <blackeder3@gmail.com>
2024-01-14 14:44:29 +0200

    chore: add model mapper dependency and bean

---
+++ src/main/java/com/graduation/system/config/MappingConfig.java
@@ -0,0 +1,16 @@
+package com.graduation.system.config;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MappingConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
